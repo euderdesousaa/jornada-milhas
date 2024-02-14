ee90b5ff05acfca0f13a0cebabc137164b1f588b
Euder de Sousa <euderdesousaaaa@gmail.com>
2024-02-14 16:35:11 -0300

    JornadaApplication

--- src/main/java/br/com/redue/jornada/JornadaApplication.java
+++ src/main/java/br/com/redue/jornada/JornadaApplication.java
@@ -3,7 +3,6 @@ package br.com.redue.jornada;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class JornadaApplication {
