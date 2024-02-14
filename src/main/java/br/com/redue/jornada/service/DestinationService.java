e7efd99de0abb6a8ca5af92ea89bbd41aebbef08
Euder de Sousa <euderdesousaaaa@gmail.com>
2024-02-14 16:32:00 -0300

    Refactoring DestinationByParam, deleteById and getDestinationById for when ID is invalid or empty, throw HttpStatus.NOT_FOUND

--- src/main/java/br/com/redue/jornada/service/DestinationService.java
+++ src/main/java/br/com/redue/jornada/service/DestinationService.java
@@ -8,10 +8,13 @@ import br.com.redue.jornada.model.dto.destination.DestinationUpdateDTO;
 import br.com.redue.jornada.repository.DestinationRepository;
 import com.pkslow.ai.GoogleBardClient;
 import com.pkslow.ai.domain.Answer;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,7 +33,7 @@ public class DestinationService {
     @Transactional
     public DestinationDto createADestination(DestinationDto dto) {
         Destination save = repository.save(mapper.toEntity(dto));
-        if(dto.getDescText().isEmpty()){
+        if (dto.getDescText().isEmpty()) {
             Answer answer = bardClient.ask("Faça um resumo sobre " + dto.getName() + " enfatizando o porque este lugar é " +
                     "incrível. Utilize uma linguagem informal e até 100 caracteres no máximo em cada parágrafo. Crie 2 parágrafos neste resumo.");
             save.setDescText(answer.getChosenAnswer());
@@ -38,8 +41,12 @@ public class DestinationService {
         return mapper.toDTO(save);
     }
 
-    public List<Destination> getDestinationByParam(@RequestParam("name") String name) {
-        return repository.findByName(name);
+    public ResponseEntity<Object> getDestinationByParam(String name) {
+        List<Destination> findByName = repository.findByName(name);
+        if (findByName.isEmpty()) {
+            return ResponseEntity.status( HttpStatus.NOT_FOUND).body("Name not found "+ name);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(findByName);
     }
 
     public List<Destination> findAll() {
@@ -47,12 +54,15 @@ public class DestinationService {
     }
 
     public DestinationByIdDto getDestinationById(Long id) {
-        Optional<Destination> destination = repository.findById(id);
+        Optional<Destination> destination = Optional.ofNullable(repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Nenhum destino foi encontrado.")));
         return new DestinationByIdDto(destination);
     }
 
+    @Transactional
     public DestinationUpdateDTO updateADestination(Long id, DestinationUpdateDTO dto) {
         Destination destination = repository.findById(id).orElse(null);
+        assert destination != null;
         destination.setTarget(dto.getTarget());
         destination.setDescText(dto.getDescText());
         Destination save = repository.save(destination);
@@ -60,7 +70,13 @@ public class DestinationService {
     }
 
     @Transactional
-    public void deleteADestinationById(Long id) {
-        repository.deleteById(id);
+    public ResponseEntity<Object> deleteADestinationById(Long id) {
+        Optional<Destination> findById = repository.findById(id);
+        if (findById.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        repository.deleteById(findById.get().getId());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Product deleted successfuly");
+
     }
 }
