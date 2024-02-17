ac3b6d24360d406ce4bf2c995bafa798e1466dce
Euder de Sousa <euderdesousaaaa@gmail.com>
2024-02-17 17:25:51 -0300

    Testimony Update Dto

---
+++ src/main/java/br/com/redue/jornada/model/dto/testimony/TestimonyUpdateDto.java
@@ -0,0 +1,15 @@
+package br.com.redue.jornada.model.dto.testimony;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TestimonyUpdateDto {
+
+    private String testimony;
+}
