2747cf9501368488d8e46297e5081b5ed8301559
Euder de Sousa <euderdesousaaaa@gmail.com>
2024-02-17 17:25:35 -0300

    Create findByID and refactoring DeleteById

--- src/main/java/br/com/redue/jornada/service/TestimonyService.java
+++ src/main/java/br/com/redue/jornada/service/TestimonyService.java
@@ -2,17 +2,17 @@ package br.com.redue.jornada.service;
 
 import br.com.redue.jornada.mapper.TestimonyMapper;
 import br.com.redue.jornada.model.Testimony;
-import br.com.redue.jornada.model.dto.TestimonyDto;
+import br.com.redue.jornada.model.dto.testimony.TestimonyDto;
+import br.com.redue.jornada.model.dto.testimony.TestimonyUpdateDto;
 import br.com.redue.jornada.repository.UserRepository;
-import br.com.redue.jornada.service.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -21,9 +21,15 @@ public class TestimonyService {
     private final UserRepository repository;
     private final TestimonyMapper mapper;
 
-    public List<Testimony> findAll(){
+    public List<Testimony> findAll() {
         return repository.findAll();
     }
+
+    public Optional<Testimony> findById(UUID id) {
+        return Optional.ofNullable(repository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Nenhum destino foi encontrado.")));
+    }
+
 
     @Transactional
     public List<Testimony> getRandomTestimony(int amount) {
@@ -43,24 +49,28 @@ public class TestimonyService {
     }
 
     @Transactional
-    public TestimonyDto insert(TestimonyDto dto){
+    public TestimonyDto insert(TestimonyDto dto) {
         Testimony save = repository.save(mapper.toEntity(dto));
         return mapper.toDTO(save);
     }
 
     @Transactional
-    public TestimonyDto update(UUID id){
+    public TestimonyUpdateDto update(UUID id, TestimonyUpdateDto dto) {
         Testimony update = repository.findById(id).orElse(null);
-        Testimony save = repository.save(mapper.toUpdate(update));
-        if(update == null)
-        {
-            throw new ResourceNotFoundException("Destination not found");
-        }
-        return mapper.toDTO(save);
+        assert update != null;
+        update.setTestimony(dto.getTestimony());
+        Testimony save = repository.save(update);
+        return mapper.toUpdate(save);
     }
 
     @Transactional
-    public void deleteTestimonyById(UUID id){
-        repository.deleteById(id);
+    public ResponseEntity<Object> deleteTestimonyById(UUID id) {
+        Optional<Testimony> findById = repository.findById(id);
+        if (findById.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        repository.deleteById(findById.get().getId());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Product deleted successfuly");
+
     }
 }
